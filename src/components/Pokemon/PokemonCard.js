6e8cfd1231c8e5c4f07d6ef6301544d3ac85b196
HapticHash <harshitks2203@gmail.com>
2021-01-31 18:47:01 -0500

    updated css

--- src/components/Pokemon/PokemonCard.js
+++ src/components/Pokemon/PokemonCard.js
@@ -5,7 +5,6 @@ import "./PokemonCard.css";
 function PokemonCard({ name, imageUrl }) {
   const [id, setId] = useState([]);
   const [type, setType] = useState([]);
-  const [types, setTypes] = useState([]);
   const [imageurls, setImageUrls] = useState([]);
   const [weight, setWeight] = useState([]);
   const [height, setHeight] = useState([]);
@@ -17,11 +16,11 @@ function PokemonCard({ name, imageUrl }) {
       const request = await axios.get(url);
       setId(request.data.id);
 
-      request.data.types.forEach((element) => {
-        setTypes(element.type.name);
-        console.log(element.type.name);
-      });
-      // setType(request.data.types[0].type.name);
+      // request.data.types.forEach((element) => {
+      //   setTypes(element.type.name);
+      //   console.log(element.type.name);
+      // });
+      setType(request.data.types[0].type.name);
       setImageUrls(request.data.sprites.other.dream_world.front_default);
       setBaseExp(request.data.base_experience);
       setHeight(request.data.height);
@@ -29,7 +28,6 @@ function PokemonCard({ name, imageUrl }) {
       // setMoves(request.data.moves);
 
       console.log(">>>", request.data);
-      // black-white.animated.front_default
       return request;
     }
     fetchData();
@@ -38,14 +36,19 @@ function PokemonCard({ name, imageUrl }) {
   return (
     <div className="col-md-3 col-sm-6 mb-5">
       <div className="card">
-        <img className="images" src={imageurls} alt={name} />
-        <h5 className="card-title">{name}</h5>
-        <div className="card-body">
+        <div className="group">
+          <img className="images" src={imageurls} alt={name} />
           <div className="id"> #{id} </div>
-          <div className="type"> {type} </div>
-          <div className="base_exp"> {baseExp} exp </div>
-          <div className="height"> {height} in </div>
-          <div className="weight"> {weight} lbs </div>
+        </div>
+
+        <div className="details">
+          <h5 className="card-title">{name}</h5>
+          <div className="card-desc">
+            <div className="type `${type}`">{type}</div>
+            <div className="base_exp"> {baseExp} exp </div>
+            <div className="height"> {height} in </div>
+            <div className="weight"> {weight} lbs </div>
+          </div>
         </div>
       </div>
     </div>
